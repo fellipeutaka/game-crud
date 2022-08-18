9cd5ffd0f8296f2df11ae471e8e30d1e773490e3
fellipeutaka <fellipeutaka@gmail.com>
2022-08-17 23:18:30 -0300

    * feat: home link to admin;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Button, Flex, Heading, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 import Seo from "@game-crud/components/Seo";
 
@@ -13,10 +14,22 @@ export default function Home() {
         alignItems="center"
         w="full"
         h="100vh"
+        flexDir="column"
+        gap="8"
       >
         <Heading as="h1" size="lg">
           Home
         </Heading>
+        <NextLink passHref href="/admin">
+          <Link
+            as={Button}
+            bgColor="blue.600"
+            _hover={{ textDecor: "none", bgColor: "blue.500" }}
+            _focus={{ bgColor: "blue.500" }}
+          >
+            Go to admin page
+          </Link>
+        </NextLink>
       </Flex>
     </Seo>
   );
